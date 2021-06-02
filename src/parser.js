ff330de0cae54e05afc8d99624f73f0fb3fa5cd5
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-02 11:14:19 +0900

    fix parser

--- src/parser.js
+++ src/parser.js
@@ -1,38 +1,7 @@
 function fakeParseJSON(str) {
   let i = 0;
-  function parseValue() {
-    skipWhiteSpace()
-    const value = 
-      parseString() ??
-      parseNumber() ??
-      parseObject() ??
-      parseArray() ??
-      parseKeyword('true', true) ?? 
-      parseKeyword('false', false) ?? 
-      parseKeyword('null', null);
-    skipWhiteSpace()
-    return value;
-  }
 
-  function parseArray() {
-    if (str[i] === '[') {
-      i++
-      skipWhiteSpace();
-
-      const result = []
-      let initial = true
-      while (str[i] !== ']') {
-        if (!initial) {
-          eatComma()
-        }
-        const value = parseValue()
-        result.push(value)
-        initial = false
-      }
-      i++
-      return result
-    }
-  }
+  return parseValue()
 
   function parseObject() {
     if (str(i) === '{') {
@@ -58,10 +27,146 @@ function fakeParseJSON(str) {
     }
   }
 
+  function parseArray() {
+    if (str[i] === '[') {
+      i++
+      skipWhiteSpace();
+
+      const result = []
+      let initial = true
+      while (str[i] !== ']') {
+        if (!initial) {
+          eatComma()
+        }
+        const value = parseValue()
+        result.push(value)
+        initial = false
+      }
+      i++
+      return result
+    }
+  }
+
+
   function parseKeyword(name, value) {
     if (str.slice(i, i + name.length) === name) {
       i += name.length;
       return value;
+    }
+  }
+
+  function parseValue() {
+    skipWhiteSpace()
+    const value = 
+      parseString() ??
+      parseNumber() ??
+      parseObject() ??
+      parseArray() ??
+      parseKeyword('true', true) ?? 
+      parseKeyword('false', false) ?? 
+      parseKeyword('null', null);
+    skipWhiteSpace()
+    return value;
+  }
+
+  function parseKeyword(name, value) {
+    if (str.slice(i, i + name.length) === name) {
+      i += name.length;
+      return value;
+    }
+  }
+
+  function skipWhiteSpace() {
+    while (
+      str[i] === " " ||
+      str[i] === "\n" ||
+      str[i] === "\t" ||
+      str[i] === "\r"
+    ) {
+      i++;
+    }
+  }
+
+  function parseString() {
+    if (str[i] === '"') {
+      i++;
+      let result = "";
+      while (str[i] !== '"') {
+        if (str[i] === "\\") {
+          const char = str[i + 1];
+          if (
+            char === '"' ||
+            char === "\\" ||
+            char === "/" ||
+            char === "b" ||
+            char === "f" ||
+            char === "n" ||
+            char === "r" ||
+            char === "t"
+          ) {
+            result += char;
+            i++;
+          } else if (char === "u") {
+            if (
+              isHexadecimal(str[i + 2]) &&
+              isHexadecimal(str[i + 3]) &&
+              isHexadecimal(str[i + 4]) &&
+              isHexadecimal(str[i + 5])
+            ) {
+              result += String.fromCharCode(
+                parseInt(str.slice(i + 2, i + 6), 16)
+              );
+              i += 5;
+            }
+          }
+        } else {
+          result += str[i];
+        }
+        i++;
+      }
+      i++;
+      return result;
+    }
+  }
+
+  function isHexadecimal(char) {
+    return (
+      (char >= "0" && char <= "9") ||
+      (char.toLowerCase() >= "a" && char.toLowerCase() <= "f")
+    );
+  }
+
+  function parseNumber() {
+    let start = i;
+    if (str[i] === "-") {
+      i++;
+    }
+    if (str[i] === "0") {
+      i++;
+    } else if (str[i] >= "1" && str[i] <= "9") {
+      i++;
+      while (str[i] >= "0" && str[i] <= "9") {
+        i++;
+      }
+    }
+
+    if (str[i] === ".") {
+      i++;
+      while (str[i] >= "0" && str[i] <= "9") {
+        i++;
+      }
+    }
+    if (str[i] === "e" || str[i] === "E") {
+      i++;
+      if (str[i] === "-" || str[i] === "+") {
+        i++;
+      }
+      while (str[i] >= "0" && str[i] <= "9") {
+        i++;
+      }
+    }
+    if (i > start) {
+      return Number(str.slice(start, i));
     }
   }
 
@@ -75,13 +180,6 @@ function fakeParseJSON(str) {
   function eatColon() {
     if (str[i] !== ':') {
       throw new Error('Expected ":".')
-    }
-    i++
-  }
-
-  function skipWhiteSpace() {
-    if (str[i] !== ' ') {
-      throw new Error('Expected "whitespace".')
     }
     i++
   }
