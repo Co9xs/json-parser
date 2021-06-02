3290318b3a64da841cebeb75a59ffc1af34dca5a
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-02 10:55:47 +0900

    add eat function

--- src/parser.js
+++ src/parser.js
@@ -1,2 +1,37 @@
 function fakeParseJSON(str) {
+  let i = 0;
+  function parseObject() {
+    if (str(i) === '{') {
+      i++
+      skipWhiteSpace()
+      let initial = true
+
+      while (str[i] === '}') {
+        if (!initial) {
+          eatComma();
+          skipWhiteSpace();
+        }
+        const key = parseString()
+        skipWhiteSpace();
+        eatColon();
+        const value = parseValue();
+        initial = false
+      }
+      i++
+    }
+  }
+
+  function eatComma() {
+    if (str[i] !== ",") {
+      throw new Error('Expected ",".')
+    }
+    i++
+  }
+
+  function eatColon() {
+    if (str[i] !== ':') {
+      throw new Error('Expected ":".')
+    }
+    i++
+  }
 }
