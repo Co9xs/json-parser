d5c7ca7841f78c4674aebc86c8004ee68a262705
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-03 15:38:03 +0900

    create example of mjs

---
+++ src/count.mjs
@@ -0,0 +1,5 @@
+// counter.js
+import { message } from "./main.mjs"
+export const count = 5;
+
+setTimeout(() => console.log(message), 0)
---
+++ src/main.mjs
@@ -0,0 +1,4 @@
+// main.js
+import { count } from './count.mjs'
+console.log(count)
+export const message = "Eval Complete";
